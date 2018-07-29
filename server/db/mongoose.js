c76a6d0d25231624d1ff9da2cb9e6ab0d84098db
Saif <saif.s.ahmed78@gmail.com>
2018-07-28 20:55:05 -0700

    Setup app for heroku test

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -4,9 +4,9 @@ mongoose.Promise = global.Promise;
 
 let db = {
   localhost: 'mongodb://localhost:27017/TodoApp',
-  mlab: 'mongodb://saifahmed:secretpass@ds259241.mlab.com:59241/todoapp'
+  mlab: 'mongodb://saifahmed:secretpass1@ds259241.mlab.com:59241/todoapp'
 };
-mongoose.connect(db.localhost || db.mlab, {useNewUrlParser: true});
+mongoose.connect(process.env.PORT? db.mlab || db.localhost, {useNewUrlParser: true});
 
 module.exports = {
   mongoose
