20da38b481c81b5d29847cd3be84f9fefe01b5e4
Saif <saif.s.ahmed78@gmail.com>
2018-07-28 20:48:14 -0700

    Setup app for heroku 1

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -4,7 +4,7 @@ mongoose.Promise = global.Promise;
 
 let db = {
   localhost: 'mongodb://localhost:27017/TodoApp',
-  mlab: 'mongodb://<user>:<password>@ds259241.mlab.com:59241/todoapp'
+  mlab: 'mongodb://saifahmed:secretpass@ds259241.mlab.com:59241/todoapp'
 };
 mongoose.connect(db.localhost || db.mlab, {useNewUrlParser: true});
 
