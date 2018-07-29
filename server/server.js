d03a0eed2250d289001c9eba4bc446eb875a068f
Saif <saif.s.ahmed78@gmail.com>
2018-07-28 19:16:33 -0700

    Added GET /todos/:id route

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 
+const {ObjectID}  = require('mongodb');
+
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
@@ -28,6 +30,23 @@ app.get('/todos', (req,res) => {
   }, (e) => {
     res.status(400).send(e);
   });
+});
+
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  if(!ObjectID.isValid(id)){
+    //console.log("Not valid ID");
+    return res.status(404).send();
+  }
+  Todo.findById(id).then((todo) => {
+    if(!todo){
+      return res.status(404).send();
+    }
+    res.send({todo});
+  }, (e) => {
+    res.status(400).send();
+  });
+  //res.send(req.params);
 });
 
 app.listen(3000, () => {
